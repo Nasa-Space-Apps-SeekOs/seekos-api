9a576cab86d6125b1adfe2a6107fd3212bd2e9be
Samuel Frederico <osamuelfrederico@gmail.com>
2023-10-07 21:53:35 -0300

    feat: :sparkles: Endpoints Repository and Comments
    Add endpoints to repositories and action to get the comments of an repository

--- app/seekosApi/views.py
+++ app/seekosApi/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from seekosApi.models import User
-from seekosApi.serializers import UserSerializer
+from seekosApi.models import User, Country, Repository, RepositoryComment
+from seekosApi.serializers import UserSerializer, CountrySerializer, RepositorySerializer, RepositoryCommentSerializer
 from django.contrib.auth.decorators import login_required
+from rest_framework.response import Response
+from rest_framework.decorators import action
 # Create your views here.
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer   
+
+
+class CountryViewSet(viewsets.ModelViewSet):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
+
+class RepositoryViewSet(viewsets.ModelViewSet):
+    queryset = Repository.objects.all()
+    serializer_class = RepositorySerializer
+
+    @action(detail=True, methods=['get'])
+    def comments(self, request, pk=None):
+        repository = self.get_object()
+        comments = repository.repositorycomment_set.all()
+        serializer = RepositoryCommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+
+class RepositoryCommentViewSet(viewsets.ModelViewSet):
+    queryset = RepositoryComment.objects.all()
+    serializer_class = RepositoryCommentSerializer

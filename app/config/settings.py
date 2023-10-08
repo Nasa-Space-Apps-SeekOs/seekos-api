32b4a405e78b5f1981a14be62e40964f0153ef70
Samuel Frederico <osamuelfrederico@gmail.com>
2023-10-08 09:20:40 -0300

    feat: :sparkles: Update ALLOWED_HOSTS config
    Update ALLOWED_HOSTS config

--- app/config/settings.py
+++ app/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$mh^q5#$x0*odnj06h&t@$&fj)cevihr_h@e29ky6uf%6vki15
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
